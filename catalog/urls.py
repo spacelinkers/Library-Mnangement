ab9ed7f666392b4a385f28c045bb7842668e0f9e
spacelinkers <josen221@gmail.com>
2018-12-27 13:34:48 +0600

    test

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('books/', views.BookListView.as_view(), name = 'books'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name = 'book-detail')
+    path('books/<int:pk>', views.BookDetailView.as_view(), name = 'book-detail')
 
 ]
